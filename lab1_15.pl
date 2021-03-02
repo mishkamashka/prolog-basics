2ddf2dd0bf237cf80a4343b6a7c85ad7544c76ae
mishkamashka <anisimova.mariia.mm@gmail.com>
2021-03-02 12:01:06 +0300

    add lab1 c

--- lab1_15.pl
+++ lab1_15.pl
@@ -12,17 +12,15 @@ man(pavel).
 % queries to identify names of fathers, mothers, grandmothers and
 % family members of families with mother, father and daughter.
 
-not(P) :- (call(P) -> fail ; true).
+not(P) :- (call(P) -> fail ; true)
 
-%woman(X) :- not(man(X)).
+% woman(X) :- not(man(X)).
 % woman(X) :- \+man(X).
-
-woman(X) :- man(X) -> fail ; true.
-
-family(X,Y,Z) :- daughter(Z,Y), man(X).
 
 fathers_names(X, Y) :- man(X), daughter(Y,X).
 
 mothers_names(X, Y) :- daughter(Y,X), not(man(X)).
 
 grandmothers_names(X,Z) :- daughter(Y,X), daughter(Z,Y), not(man(X)).
+
+family(X,Y,Z) :- daughter(Z,Y), daughter(Z,X), man(X), X \= Y.
