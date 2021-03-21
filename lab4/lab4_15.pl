afe824e69ba0b5496e23c300fd46a04580502fab
mishkamashka <anisimova.mariia.mm@gmail.com>
2021-03-21 13:52:42 +0300

    lab4 finished

--- lab4/lab4_15.pl
+++ lab4/lab4_15.pl
@@ -1,2 +1,13 @@
 % 15 Определить предикат, который для дерева T определят количество
 % «листьев».
+
+
+count_leaves(nil, Res) :- Res is 0.
+
+count_leaves(tr(_, nil, nil), Res) :- Res is 1.
+
+count_leaves(tree(_, nil, Right), Res) :- count_leaves(Right, Res).
+
+count_leaves(tree(_, Left, nil), Res) :- count_leaves(Left, Res).
+
+count_leaves(tr(_,Left,Right), Res) :- count_leaves(Left, Res1), count_leaves(Right, Res2), Res is Res1 + Res2.
--- lab4/lab4_6.pl
+++ lab4/lab4_6.pl
@@ -1,2 +1,7 @@
 % 6 Определить предикат, который для двух деревьев S и T определяет
 % является ли S непустым поддеревом T .
+
+check(Tree1, Tree2):- Tree1 = Tree2, !.
+check(tr(_,Left,Right), Tree2) :- check(Left, Tree2); check(Right,Tree2).
+
+% ?- check(tr(5,tr(7,tr(9,nil,nil),tr(8,nil,nil)),tr(6,nil,tr(4,tr(3,nil,nil),tr(1,nil,nil)))), tr(4,tr(3,nil,nil),tr(1,nil,nil))).
