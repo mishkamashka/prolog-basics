7ff9a90fca9577eaef45931f891e2843e06f43fd
mishkamashka <anisimova.mariia.mm@gmail.com>
2021-03-23 19:58:58 +0300

    lab5.6

--- lab5/lab5_6.pl
+++ lab5/lab5_6.pl
@@ -1,2 +1,14 @@
 % 6 Определить предикат, который возвращает строку, получаемую из
 % исходной строки удалением всех вхождений заданной подстроки.
+
+
+rm_substr(String, Substring, Res) :- has_substring(String, Substring), re_replace(Substring, '', String, Res1), rm_substr(Res1, Substring, Res), !.
+rm_substr(String, Substring, Res) :- \+has_substring(String, Substring), Res = String, !.
+
+has_substring(String, Substring) :-
+  atom_chars(String,List1), atom_chars(Substring,List2), my_sublist(List1, List2).
+
+my_sublist( List, Sublist ) :-
+    append( [_, Sublist, _], List ).
+
+% re_replace(+Pattern, +With, +String, -NewString).
