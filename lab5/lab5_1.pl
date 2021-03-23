73dccd95ef0a8d12c34034d1296d155121ba09e8
mishkamashka <anisimova.mariia.mm@gmail.com>
2021-03-23 19:01:10 +0300

    add lab5.1

--- lab5/lab5_1.pl
+++ lab5/lab5_1.pl
@@ -1,2 +1,10 @@
 % 1 Определить предикат, который находит количество вхождений
 % символа в строку.
+
+count(Char, String, Res) :- atom_chars(String, List), count_chars(Char, List, Res).
+
+count_chars(_, [], Res) :- Res is 0.
+
+count_chars(Char, [Head|Tail], Res) :- Char = Head, count_chars(Char, Tail, Res1), Res is  Res1 + 1.
+
+count_chars(Char, [Head|Tail], Res) :- Char \= Head, count_chars(Char, Tail, Res1), Res = Res1.
