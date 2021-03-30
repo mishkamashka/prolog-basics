b5ffcad70f21bce27638f579d8bac5860dd76c03
mishkamashka <anisimova.mariia.mm@gmail.com>
2021-03-30 11:14:50 +0300

    fix lab1

--- lab1/lab1_10.pl
+++ lab1/lab1_10.pl
@@ -1,10 +1,18 @@
 # vim: ft=prolog
 
-wings(X) :- bird(X);plane(X).
 bird(crow).
 bird(penguin).
 bird(peacock).
 bird(parrot).
+bird(ostrich).
+
+wings(crow).
+wings(penguin).
+wings(parrot).
+wings(peacock).
+wings(ostrich).
+wings(airbus).
+
 
 flies(X) :- bird(X), wings(X), dif(ostrich, X), dif(penguin, X).
 
